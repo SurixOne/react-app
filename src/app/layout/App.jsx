25270e1fe695baf28f42dc9407c700a43db6d31f
ealvarez <ealvarez@symplr.com>
2021-12-14 01:22:04 -0300

    added form and list of events

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { useState } from "react/cjs/react.development";
+import { Container } from "semantic-ui-react";
+import EventDashboard from "../../features/events/eventDashboard/EventDashboard";
+import NavBar from "../../features/nav/NavBar";
+
+
 function App() {
+  const [formOpen, setFormOpen] = useState(false);
   return (
-    <div className="App">
-      <h1>surix app</h1>
-    </div>
+    <>
+      <NavBar setFormOpen={setFormOpen}></NavBar>
+      <Container className="main">
+        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} />
+      </Container>
+    </>
   );
 }
 
