14b8fff5cd308a0e12b86707188d34ba365cb042
ealvarez <ealvarez@symplr.com>
2021-12-19 21:55:30 -0300

    made homepage

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import { useState } from "react/cjs/react.development";
 import { Container } from "semantic-ui-react";
 import EventDashboard from "../../features/events/eventDashboard/EventDashboard";
+import EventDetailPage from "../../features/events/eventDetail/EventDetailPage";
+import EventForm from "../../features/events/eventForm/EventForm";
+import HomePage from "../../features/home/HomePage";
 import NavBar from "../../features/nav/NavBar";
 
 function App() {
@@ -18,17 +22,29 @@ function App() {
     setFormOpen(true);
   }
 
+  //   <EventDashboard
+  //   formOpen={formOpen}
+  //   setFormOpen={setFormOpen}
+  //   selectEvent={handleSelectEvent}
+  //   selectedEvent={selectedEvent}
+  // />
   return (
     <>
-      <NavBar setFormOpen={handleCreateFormOpen}></NavBar>
-      <Container className="main">
-        <EventDashboard
-          formOpen={formOpen}
-          setFormOpen={setFormOpen}
-          selectEvent={handleSelectEvent}
-          selectedEvent={selectedEvent}
-        />
-      </Container>
+      <Route exact path="/" component={HomePage} />
+      <Route
+        path={"/(.+)"}
+        render={() => (
+          <>
+            <NavBar setFormOpen={handleCreateFormOpen}></NavBar>
+            <Container className="main">
+              <Route exact path="/" component={HomePage} />
+              <Route exact path="/events" component={EventDashboard} />
+              <Route path="/events:id" component={EventDetailPage} />
+              <Route path="/createEvent" component={EventForm} />
+            </Container>
+          </>
+        )}
+      />
     </>
   );
 }
--- src/features/nav/NavBar.jsx
+++ src/features/nav/NavBar.jsx
@@ -1,23 +1,30 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import { Button, Container, Menu } from "semantic-ui-react";
+import SignedInMenu from "../home/signedInMenu/SignedInMenu";
+import SignedOutMenu from "../home/signedOutMenu/SignedOutMenu";
 
-export default function NavBar({setFormOpen}) {
-    return(
-        <Menu inverted fixed="top">
-            <Container>
-                <Menu.Item>
-                    <img src="/assets/logo.png" alt="logo" style={{marginRight: 10}}/>
-                    Que sale hoy
-                </Menu.Item>
-                <Menu.Item name="Events" />
-                <Menu.Item>
-                    <Button onClick={() => setFormOpen(true)} positive inverted content="Create Event" />
-                </Menu.Item>
-                <Menu.Item position="right">
-                    <Button basic inverted content="Login" />
-                    <Button basic inverted content="Register" style={{marginLeft: "0.5em"}} />
-                </Menu.Item>
-            </Container>
-        </Menu>
-    )
+export default function NavBar({ setFormOpen }) {
+  return (
+    <Menu inverted fixed="top">
+      <Container>
+        <Menu.Item as={NavLink} exact to="/">
+          <img src="/assets/logo.png" alt="logo" style={{ marginRight: 10 }} />
+          Que sale hoy
+        </Menu.Item>
+        <Menu.Item as={NavLink} to="/events" name="Events" />
+        <Menu.Item>
+          <Button
+            as={NavLink}
+            to="/createEvent"
+            positive
+            inverted
+            content="Create Event"
+          />
+        </Menu.Item>
+        <SignedOutMenu />
+        <SignedInMenu />
+      </Container>
+    </Menu>
+  );
 }
